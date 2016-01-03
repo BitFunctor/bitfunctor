f4e1d89028dc0ce92565df41ba421d5178322540
Sergey Kazenyuk <kazenyuk@gmail.com>
2016-01-03 07:24:49 +0300

    Transaction value

--- src/Network/BitFunctor/Transaction.hs
+++ src/Network/BitFunctor/Transaction.hs
@@ -1,6 +1,7 @@
 module Network.BitFunctor.Transaction ( Transaction (..)
                                       , sign
                                       , verify
+                                      , value
                                       ) where
 
 import qualified Crypto.PubKey.Ed25519 as C (sign, verify)
@@ -8,6 +9,7 @@ import qualified Data.ByteString as B
 
 import Network.BitFunctor.Transaction.Types
 import Network.BitFunctor.Account
+import Network.BitFunctor.Token (BTF, (+.))
 
 
 sign :: Account -> Transaction -> Maybe Transaction
@@ -20,6 +22,10 @@ sign acc tx = Nothing
 verify :: Transaction -> Bool
 verify tx = C.verify (pubKey . fromAccountId $ sender tx)
                      (signEncode tx) $ signature tx
+
+
+value :: Transaction -> BTF
+value tx = amount tx +. fee tx
 
 
 signEncode :: Transaction -> B.ByteString
