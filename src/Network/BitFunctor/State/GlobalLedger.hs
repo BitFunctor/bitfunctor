53d658e1fe980cad9ab5652bc1ba1106cb5fbb38
Sergey Kazenyuk <kazenyuk@gmail.com>
2016-01-03 07:56:40 +0300

    GlobalLedger

---
+++ src/Network/BitFunctor/State/GlobalLedger.hs
@@ -0,0 +1,23 @@
+module Network.BitFunctor.State.GlobalLedger ( GlobalLedger
+                                             , accountBalance
+                                             , ledger
+                                             ) where
+
+import qualified Network.BitFunctor.State.Ledger as L
+import Network.BitFunctor.Account
+import Network.BitFunctor.Token
+import Network.BitFunctor.Block
+
+import qualified Data.Map as M
+
+
+type GlobalLedger = M.Map BlockHash L.Ledger
+
+
+accountBalance :: GlobalLedger -> BlockHash -> AccountId -> Maybe BTF
+accountBalance gl h acc = do
+  le <- ledger gl h
+  return $ L.accountBalance le acc
+
+ledger :: GlobalLedger -> BlockHash -> Maybe L.Ledger
+ledger = flip M.lookup
