89a33b7a667b44a137fef7aca73243760da5f0ae
Sergey Kazenyuk <kazenyuk@gmail.com>
2016-04-14 19:16:07 +0300

    Arbitrary instances for PublicKey and Signature

--- test/src/Network/BitFunctor/Crypto/Arbitrary.hs
+++ test/src/Network/BitFunctor/Crypto/Arbitrary.hs
@@ -6,7 +6,7 @@ import Network.BitFunctor.Crypto.Types
 
 import Data.ByteArray (pack, Bytes)
 import Data.Word
-import Crypto.PubKey.Ed25519 (secretKey)
+import Crypto.PubKey.Ed25519 (secretKey, toPublic, sign)
 import Crypto.Error
 
 import Test.QuickCheck
@@ -17,4 +17,16 @@ instance Arbitrary SecretKey where
   arbitrary = do
     let secKeyLength = 32 -- cryptonite doesn't export that...
     bytes <- vectorOf secKeyLength (arbitrary :: Gen Word8)
-    return . throwCryptoError . secretKey $ (pack bytes :: Bytes)
+    return . throwCryptoError $ secretKey (pack bytes :: Bytes)
+
+instance Arbitrary PublicKey where
+  arbitrary = do
+    sk <- arbitrary
+    return $ toPublic sk
+
+instance Arbitrary Signature where
+  arbitrary = do
+    sk <- arbitrary
+    let pk = toPublic sk
+    bytes <- vectorOf 1 (arbitrary :: Gen Word8)
+    return $ sign sk pk (pack bytes :: Bytes)
