985668cbc6ff2fd3c841846695bdb530881e7c6f
Sergey Kazenyuk <kazenyuk@gmail.com>
2016-04-14 19:14:00 +0300

    Binary instances for AccountId and Signature

--- src/Network/BitFunctor/Crypto/Types.hs
+++ src/Network/BitFunctor/Crypto/Types.hs
@@ -4,9 +4,13 @@ module Network.BitFunctor.Crypto.Types where
 
 import qualified Crypto.PubKey.Ed25519 as Ed25519
 import Data.Aeson
+import Data.Binary
 import Data.ByteArray (convert)
 import qualified Data.ByteString.Base16 as B16 (encode, decode)
 import qualified Data.Text.Encoding as TE
+import qualified Data.ByteString as B
+import Data.ByteArray (pack, Bytes)
+import Crypto.Error
 
 
 type SecretKey = Ed25519.SecretKey
@@ -17,5 +21,14 @@ type Signature = Ed25519.Signature
 instance ToJSON PublicKey where
   toJSON = String . TE.decodeUtf8 . B16.encode . convert
 
+instance Binary PublicKey where
+  put pk = put $ (convert pk :: B.ByteString)
+  get    = get >>= \bytes -> return . throwCryptoError . Ed25519.publicKey $ (pack bytes :: Bytes)
+
+
 instance ToJSON Signature where
   toJSON = String . TE.decodeUtf8 . B16.encode . convert
+
+instance Binary Signature where
+  put sig = put $ (convert sig :: B.ByteString)
+  get     = undefined
