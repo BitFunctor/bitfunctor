a8638e11b6c8c167152c95431eb663183d2a6539
Sergey Kazenyuk <kazenyuk@gmail.com>
2016-04-14 04:08:53 +0300

    A couple more AccountId-related properties

--- test/src/Network/BitFunctor/Account/Tests.hs
+++ test/src/Network/BitFunctor/Account/Tests.hs
@@ -12,8 +12,15 @@ import Test.HUnit (Assertion, (@?=))
 tests :: TestTree
 tests = testGroup "Network.BitFunctor.Account.Tests"
   [ testProperty "from_accountid(to_accountid(account)).pubkey == account.pubkey" prop_accountid_inv_over_pk
+  , testProperty "from_accountid(to_accountid(account)).seckey == Nothing" prop_accountid_erases_sk
+  , testProperty "to_accountid(from_accountid(accountid)) == accountid" prop_accountid_inv_over_accountid
   ]
 
 prop_accountid_inv_over_pk :: Account -> Bool
 prop_accountid_inv_over_pk acc = pubKey (fromAccountId $ toAccountId acc) == pubKey acc
 
+prop_accountid_erases_sk :: Account -> Bool
+prop_accountid_erases_sk acc = secKey (fromAccountId $ toAccountId acc) == Nothing
+
+prop_accountid_inv_over_accountid :: AccountId -> Bool
+prop_accountid_inv_over_accountid accId = (toAccountId . fromAccountId) accId == accId
