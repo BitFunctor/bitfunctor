021d5f9c4fe5078dd0ace7d39a03567f0cff32be
Sergey Kazenyuk <kazenyuk@gmail.com>
2016-04-14 19:21:43 +0300

    Fix warnings

--- test/src/Network/BitFunctor/Account/Arbitrary.hs
+++ test/src/Network/BitFunctor/Account/Arbitrary.hs
@@ -1,10 +1,10 @@
 module Network.BitFunctor.Account.Arbitrary where
 
 import Network.BitFunctor.Account
-import Network.BitFunctor.Crypto.Arbitrary
+import Network.BitFunctor.Crypto.Arbitrary()
 
 import Test.QuickCheck
-import Test.QuickCheck.Instances
+import Test.QuickCheck.Instances()
 
 
 instance Arbitrary Account where
