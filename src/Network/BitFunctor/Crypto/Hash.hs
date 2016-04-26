5c637ff947a62ba665c8a5ad70161c0927922ee7
Sergey Kazenyuk <kazenyuk@gmail.com>
2016-04-26 15:17:41 +0300

    Binary and Serialize instances for Hash, module refactoring

--- src/Network/BitFunctor/Crypto/Hash.hs
+++ src/Network/BitFunctor/Crypto/Hash.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module Network.BitFunctor.Crypto.Hash ( HashAlgorithm (..)
                                       , Hash
                                       , Id
@@ -8,48 +5,9 @@ module Network.BitFunctor.Crypto.Hash ( HashAlgorithm (..)
                                       ) where
 
 import Network.BitFunctor.Crypto.Hash.Types
--- import Data.ByteArray
-import qualified Crypto.Hash as H (hash, Digest, digestFromByteString)
-import Data.Binary (Binary(..)
-                    , putWord8
-                    , getWord8)
-import qualified Data.ByteString.Base16 as B16 (encode, decode)
 import Data.ByteArray
-import Data.Word
-import qualified Data.Serialize as DS
-import qualified Data.ByteString as BS
-import GHC.Generics
+import qualified Crypto.Hash as H (hash)
 
-
-type Id = Keccak_256
-
--- instance Binary Id
-
-instance Binary (H.Digest Id) where
- put _ = putWord8 0
- get  = do
-    tag_ <- getWord8
-    case tag_ of
-      0 -> return undefined
-      _ -> fail "no parse"
- 
-instance Binary (Hash Id) where
-   put (Hash d) = put . B16.encode $ convert d
-   get = get >>= \a -> return (Hash a)
-
-instance DS.Serialize (H.Digest Id) where
- put d = DS.put $ BS.pack $ unpack d
- get  = do
-          a <- DS.get
-          let mdig = H.digestFromByteString (a :: BS.ByteString)
-          case mdig of
-            Nothing -> fail "serialize: no readable context"
-            Just dig -> return dig
-
-
-instance DS.Serialize (Hash Id) where
- put (Hash d) = DS.put d 
- get = DS.get >>= \a -> return (Hash a)
 
 
 hash :: (ByteArrayAccess ba, HashAlgorithm a) => ba -> Hash a
--- src/Network/BitFunctor/Crypto/Hash/Types.hs
+++ src/Network/BitFunctor/Crypto/Hash/Types.hs
@@ -1,25 +1,64 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Network.BitFunctor.Crypto.Hash.Types ( HashAlgorithm (..)
+                                            , Id
                                             , Keccak_256
                                             , Hash (..)
                                             , toString
                                             ) where
 
 import Crypto.Hash.Algorithms (HashAlgorithm, Keccak_256)
-import Crypto.Hash (Digest)
-import Data.Aeson
-import Data.ByteArray (convert)
-import qualified Data.ByteString.Base16 as B16 (encode, decode)
-import qualified Data.Text.Encoding as TE
-import qualified Data.Text as DT (unpack)
-import GHC.Generics
+import Crypto.Hash (hash, Digest, digestFromByteString)
 
-data HashAlgorithm a =>
-     Hash a = Hash (Digest a)
+import Data.Aeson
+import Data.Binary (Binary(..))
+import qualified Data.Serialize as DS
+
+import Data.ByteArray (convert)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Base16 as B16 (encode, decode)
+import qualified Data.Text as DT (unpack)
+import qualified Data.Text.Encoding as TE
+
+
+
+type Id = Keccak_256
+
+
+data Hash a = Hash (Digest a)
               deriving (Eq, Ord, Show)
 
-instance (HashAlgorithm a) => ToJSON (Hash a) where
-  toJSON (Hash d) = String $ toText d
 
-toString (Hash d) = DT.unpack $ toText d
+instance ToJSON (Hash a) where
+  toJSON = String . toText
 
-toText d = TE.decodeUtf8 . B16.encode $ convert d
+
+instance (HashAlgorithm a) => Binary (Digest a) where
+  put d = put (convert d :: ByteString)
+  get = do
+    bytes <- get
+    case digestFromByteString (bytes :: ByteString) of
+      Just d  -> return d
+      Nothing -> fail "binary: can't parse digest"
+
+instance (HashAlgorithm a) => Binary (Hash a) where
+  put (Hash digest) = put digest
+  get = get >>= \algo -> return $ Hash algo
+
+
+instance DS.Serialize (Digest Id) where
+ put d = DS.put (convert d :: ByteString)
+ get   = do
+    bytes <- DS.get
+    case digestFromByteString (bytes :: ByteString) of
+      Just d  -> return d
+      Nothing -> fail "binary: can't parse digest"
+
+instance DS.Serialize (Hash Id) where
+ put (Hash digest) = DS.put digest
+ get = DS.get >>= \algo -> return (Hash algo)
+
+
+toString = DT.unpack . toText
+toText (Hash d) = TE.decodeUtf8 . B16.encode $ convert d
