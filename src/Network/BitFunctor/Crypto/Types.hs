9317029077ed09d507d2d59a1d09bac6d0722f76
Sergey Kazenyuk <kazenyuk@gmail.com>
2016-04-26 16:02:27 +0300

    Binary instance for signature

--- src/Network/BitFunctor/Crypto/Types.hs
+++ src/Network/BitFunctor/Crypto/Types.hs
@@ -3,14 +3,16 @@
 module Network.BitFunctor.Crypto.Types where
 
 import qualified Crypto.PubKey.Ed25519 as Ed25519
+import Crypto.Error
+
 import Data.Aeson
 import Data.Binary
-import Data.ByteArray (convert)
+
+import Data.ByteArray (convert, pack, Bytes)
 import qualified Data.ByteString.Base16 as B16 (encode, decode)
 import qualified Data.Text.Encoding as TE
 import qualified Data.ByteString as B
-import Data.ByteArray (pack, Bytes)
-import Crypto.Error
+
 
 
 type SecretKey = Ed25519.SecretKey
@@ -18,17 +20,22 @@ type PublicKey = Ed25519.PublicKey
 type Signature = Ed25519.Signature
 
 
+
 instance ToJSON PublicKey where
   toJSON = String . TE.decodeUtf8 . B16.encode . convert
 
 instance Binary PublicKey where
-  put pk = put $ (convert pk :: B.ByteString)
-  get    = get >>= \bytes -> return . throwCryptoError . Ed25519.publicKey $ (pack bytes :: Bytes)
+  put pk = put (convert pk :: B.ByteString)
+  get    = do
+    bytes <- get
+    return . throwCryptoError $ Ed25519.publicKey (pack bytes :: Bytes)
 
 
 instance ToJSON Signature where
   toJSON = String . TE.decodeUtf8 . B16.encode . convert
 
 instance Binary Signature where
-  put sig = put $ (convert sig :: B.ByteString)
-  get     = undefined
+  put sig = put (convert sig :: B.ByteString)
+  get     = do
+    bytes <- get
+    return . throwCryptoError $ Ed25519.signature (pack bytes :: Bytes)
