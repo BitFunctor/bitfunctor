9d49b3e40990a11cd4844f9a1c339c327babc9ab
Sergey Kazenyuk <kazenyuk@gmail.com>
2016-04-27 21:15:54 +0300

    Block test module and verifysign property

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Test.Tasty (defaultMain, testGroup)
 
 import qualified Network.BitFunctor.Account.Tests
+import qualified Network.BitFunctor.Block.Tests
 import qualified Network.BitFunctor.Transaction.Tests
 import qualified Network.BitFunctor.Tests
 
@@ -10,6 +11,7 @@ import qualified Network.BitFunctor.Tests
 main :: IO ()
 main = defaultMain $ testGroup "Tests"
   [ Network.BitFunctor.Account.Tests.tests
+  , Network.BitFunctor.Block.Tests.tests
   , Network.BitFunctor.Transaction.Tests.tests
   , Network.BitFunctor.Tests.tests
   ]
