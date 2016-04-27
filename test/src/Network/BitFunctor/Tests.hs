66bf1a77ecec405ec7a4cbd388f52f78a57433c4
Sergey Kazenyuk <kazenyuk@gmail.com>
2016-04-27 03:27:51 +0300

    One more eq over binary_encode property check
    Should probably be checked in a more clever way – it seems it
    may not find a property violation now because (o1 == o2) may
    occur too rarely

--- test/src/Network/BitFunctor/Tests.hs
+++ test/src/Network/BitFunctor/Tests.hs
@@ -27,10 +27,14 @@ tests = testGroup "Network.BitFunctor.Tests"
   , testProperty "binary_decode(binary_encode(hash_id)) == hash_id" (prop_binary_encdec_inv :: Hash Id -> Bool)
   , testProperty "binary_decode(binary_encode(tx)) == tx" (prop_binary_encdec_inv :: Transaction -> Bool)
   , testProperty "binary_decode(binary_encode(block)) == block" (prop_binary_encdec_inv :: Block -> Bool)
+  , testProperty "h1 == h2 => binary_encode(h1) == binary_encode(h2)" (prop_binary_eq_over_enc :: Hash Id -> Hash Id -> Bool)
   ]
 
 prop_binary_encdec_inv :: (Binary a, Eq a) => a -> Bool
 prop_binary_encdec_inv obj = (decode . encode) obj == obj
+
+prop_binary_eq_over_enc :: (Binary a, Eq a) => a -> a -> Bool
+prop_binary_eq_over_enc o1 o2 = (o1 == o2) <= (encode o1 == encode o2)
 
 
 instance Arbitrary Block where
